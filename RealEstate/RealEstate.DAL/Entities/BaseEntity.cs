ed547ad95c8aeeee4399f5bb02b4032e9d0ceda9
Podlipny Vadim <kyerrrenmgt@gmail.com>
2025-04-02 12:15:59 +0300

    Add CreatedAt and UpdatedAt props

--- RealEstate/RealEstate.DAL/Entities/BaseEntity.cs
+++ RealEstate/RealEstate.DAL/Entities/BaseEntity.cs
@@ -3,5 +3,7 @@
     public abstract class BaseEntity
     {
         public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
