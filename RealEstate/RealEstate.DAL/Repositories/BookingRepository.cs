c7a11e936d1cf3a1741ae5d3bfc81378185fc718
Podlipny Vadim <kyerrrenmgt@gmail.com>
2025-04-03 14:45:48 +0300

    Add remaining repositories

---
+++ RealEstate/RealEstate.DAL/Repositories/BookingRepository.cs
@@ -0,0 +1,10 @@
+﻿using RealEstate.DAL.Entities;
+using RealEstate.DAL.Interfaces;
+
+namespace RealEstate.DAL.Repositories
+{
+    public class BookingRepository : BaseRepository<BookingEntity>, IBookingRepository
+    {
+        public BookingRepository(AppDbContext context) : base(context) { }
+    }
+}
---
+++ RealEstate/RealEstate.DAL/Repositories/HistoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using RealEstate.DAL.Interfaces;
+using RealEstate.DAL.Entities;
+
+namespace RealEstate.DAL.Repositories
+{
+    public class HistoryRepository : BaseRepository<HistoryEntity>, IHistoryRepository
+    {
+        public HistoryRepository(AppDbContext context) : base(context) { }
+    }
+}
