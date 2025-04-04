529a9cda656a58fe6951915a4d7fd40717a5e76c
Podlipny Vadim <kyerrrenmgt@gmail.com>
2025-04-04 12:06:55 +0300

    Add basic models for BLL

---
+++ RealEstate/RealEstate.BLL/Models/RealEstateModel.cs
@@ -0,0 +1,15 @@
+﻿using RealEstate.DAL.Enums;
+
+namespace RealEstate.BLL.Models
+{
+    public class RealEstateModel : BaseModel
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string Address { get; set; } = null!;
+        public EstateType EstateType { get; set; }
+        public EstateStatus EstateStatus { get; set; }
+        public Guid OwnerId { get; set; }
+    }
+}
