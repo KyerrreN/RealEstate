05db050065e3af529d4f0169f9c92a22ec5230e5
Podlipny Vadim <kyerrrenmgt@gmail.com>
2025-04-22 12:08:58 +0300

    Add two tests for Create method in GenericService

--- RealEstate/RealEstate.BLLTests/UserServiceTests.cs
+++ RealEstate/RealEstate.BLLTests/UserServiceTests.cs
@@ -13,38 +13,55 @@ namespace RealEstate.BLLTests
     {
         private readonly IUserService _userService;
         private readonly IBaseRepository<UserEntity> _repositoryMock;
+        private readonly UserEntity _firstUserEntity;
+        private readonly UserEntity _secondUserEntity;
+        private readonly UserModel _firstUserModel;
 
         public UserServiceTests()
         {
             _repositoryMock = Substitute.For<IBaseRepository<UserEntity>>();
             _userService = new UserService(_repositoryMock);
+            _firstUserEntity = new UserEntity
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "johndoe@gmail.com",
+                Phone = "+375151234567"
+            };
+            _secondUserEntity = new UserEntity
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Mark",
+                LastName = "Someone",
+                Email = "marksomeone@gmail.com",
+                Phone = "+375151234568"
+            };
+            _firstUserModel = new UserModel
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "johndoe@gmail.com",
+                Phone = "+375151234567"
+            };
         }
 
         [Fact]
         public async Task GetByIdAsync_ShouldReturnUserModel_WhenUserExists()
         {
             // arrange
-            var id = Guid.NewGuid();
-
-            var userEntity = new UserEntity
-            {
-                Id = id,
-                FirstName = "John",
-                LastName = "Doe",
-                Email = "johndoe@gmail.com",
-                Phone = "+375151234567"
-            };
-            _repositoryMock.FindByIdAsync(id, Arg.Any<CancellationToken>())
-                .Returns(userEntity);
+            _repositoryMock.FindByIdAsync(_firstUserEntity.Id, Arg.Any<CancellationToken>())
+                .Returns(_firstUserEntity);
 
             // act
-            var result = await _userService.GetByIdAsync(id, CancellationToken.None);
+            var result = await _userService.GetByIdAsync(_firstUserEntity.Id, CancellationToken.None);
 
             // assert
             result.ShouldNotBeNull();
             result.ShouldBeOfType<UserModel>();
-            result.Id.ShouldBe(id);
-            result.FirstName.ShouldBe(userEntity.FirstName);
+            result.Id.ShouldBe(_firstUserEntity.Id);
+            result.FirstName.ShouldBe(_firstUserEntity.FirstName);
         }
 
         [Fact]
@@ -67,24 +84,7 @@ namespace RealEstate.BLLTests
         public async Task GetAllAsync_ShouldReturnListUserModel_WhenThereAreUsers()
         {
             // arrange
-            List<UserEntity> userEntities = [
-                new UserEntity
-                {
-                    Id = Guid.NewGuid(),
-                    FirstName = "John",
-                    LastName = "Doe",
-                    Email = "johndoe@gmail.com",
-                    Phone = "+375151234567"
-                },
-                new UserEntity
-                {
-                    Id = Guid.NewGuid(),
-                    FirstName = "Mark",
-                    LastName = "Someone",
-                    Email = "marksomeone@gmail.com",
-                    Phone = "+375151234568"
-                }
-            ];
+            List<UserEntity> userEntities = [_firstUserEntity, _secondUserEntity];
 
             _repositoryMock.GetAllAsync(Arg.Any<CancellationToken>())
                 .Returns(userEntities);
@@ -115,6 +115,39 @@ namespace RealEstate.BLLTests
             // assert
             result.ShouldNotBeNull();
             result.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldReturnCreatedModel_WhenModelProvided()
+        {
+            // arrange
+            _repositoryMock.CreateAsync(Arg.Any<UserEntity>(), Arg.Any<CancellationToken>())
+                .Returns(callInfo =>
+                {
+                    var inputEntity = callInfo.Arg<UserEntity>();
+                    return inputEntity;
+                });
+
+            // act
+            var result = await _userService.CreateAsync(_firstUserModel, CancellationToken.None);
+
+            // assert
+            result.ShouldNotBeNull();
+            result.ShouldBeEquivalentTo(_firstUserModel);
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldThrowBadRequestException_WhenModelIsNull()
+        {
+            // arrange
+            UserModel? model = null;
+
+            // act
+            var exception = await Record.ExceptionAsync(() => _userService.CreateAsync(model, CancellationToken.None));
+            
+            // assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<BadRequestException>();
         }
     }
 }
