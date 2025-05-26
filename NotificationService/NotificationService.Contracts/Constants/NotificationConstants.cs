6278c63bad9dccf3f37a45098dd55749473b36d0
Podlipny Vadim <kyerrrenmgt@gmail.com>
2025-05-26 11:14:10 +0300

    Bump NuGet. Add RabbitMQ constants

---
+++ NotificationService/NotificationService.Contracts/Constants/NotificationConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace NotificationService.Contracts.Constants
+{
+    public class NotificationConstants
+    {
+        public const string Exchange = "notification-exchange";
+        public const string ExchangeType = "direct";
+        public const string UserRoutingKey = "user";
+        public const string RealEstateAddedRoutingKey = "real-estate-added";
+    }
+}
