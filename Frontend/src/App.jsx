7bc425fd93e08e657125be3966dff58293167f16
Podlipny Vadim <kyerrrenmgt@gmail.com>
2025-06-27 14:03:34 +0300

    Revert "Add UserInfoBlock component"
    This reverts commit f2cea53de1aa491f5050a8df248f029092240842.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RealEstate from "./Pages/RealEstate/RealEstate";
-import UserPage from "./Pages/User/UserPage";
 
 function App() {
     return (
@@ -9,7 +8,6 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<RealEstate />} />
-                    <Route path="/user" element={<UserPage />} />
                 </Routes>
             </BrowserRouter>
         </div>
