3c1782da1019871feaec485f3a391d11f682c1f1
Podlipny Vadim <kyerrrenmgt@gmail.com>
2025-07-01 17:45:07 +0300

    Add project

---
+++ ChatService/ChatService.API/Program.cs
@@ -0,0 +1,27 @@
+
+namespace ChatService.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddAuthorization();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.Run();
+        }
+    }
+}
