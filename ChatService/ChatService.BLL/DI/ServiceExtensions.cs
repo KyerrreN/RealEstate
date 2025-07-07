8acaad89329b6f153e902c2ae7371f755193d241
Podlipny Vadim <kyerrrenmgt@gmail.com>
2025-07-07 14:13:58 +0300

    Add gRPC client

--- ChatService/ChatService.BLL/DI/ServiceExtensions.cs
+++ ChatService/ChatService.BLL/DI/ServiceExtensions.cs
@@ -1,7 +1,9 @@
-﻿using ChatService.BLL.Interface;
+﻿using ChatService.BLL.Grpc;
+using ChatService.BLL.Interface;
 using ChatService.BLL.Service;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RealEstate.Grpc;
 using СhatService.DAL.DI;
 
 namespace ChatService.BLL.DI
@@ -13,6 +15,13 @@ namespace ChatService.BLL.DI
             await services.RegisterDAL(configuration);
 
             services.AddScoped<IMessageService, MessageService>();
+
+
+            services.AddGrpcClient<RealEstateService.RealEstateServiceClient>(opt =>
+            {
+                opt.Address = new Uri("https://localhost:7283");
+            });
+            services.AddScoped<RealEstateGrpcClient>();
         }
     }
 }
---
+++ ChatService/ChatService.Domain/Models/RealEstateModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatService.Domain.Models
+{
+    public class RealEstateModel
+    {
+        public required string Title { get; set; }
+        public required double Price { get; set; }
+    }
+}
